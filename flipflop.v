6e45881cf783080ead2133860fe9ceefb574e97f
Mani-0441 <133799257+Mani-0441@users.noreply.github.com>
2025-01-07 15:34:19 +0530

    Update flipflop.v
    rtl & tb

--- flipflop.v
+++ flipflop.v
@@ -10,3 +10,25 @@ module d_flipflop(d,clk,rst,q);
     end
   end
 endmodule
+
+module d_flipflop_tb();
+  reg d,clk,rst;
+  wire q;
+  d_flipflop dut(d,clk,rst,q);
+  always begin
+    #5 clk = ~clk;
+  end
+  initial begin
+    clk = 1'b0;
+    #10;
+    rst = 1'b1;
+    #15;
+    rst = 1'b0;
+    d = 1'b1;
+    #20;
+    d = 1'b0;
+    #40
+    d = 1'b1;
+    #20 $finish;
+  end
+endmodule
